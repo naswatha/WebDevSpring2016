0f09a3187e917e944708b657cba99637cc2e87ae
naswatha <naswatha@ccs.neu.edu>
2016-03-01 16:35:07 -0500

    implemented the forms service.

--- public/assignment/services/forms.service.client.js
+++ public/assignment/services/forms.service.client.js
@@ -30,22 +30,48 @@
         ];
 
 
-
-
         FormService.createFormForUser = function(userId, form, callback) {
+            form._id = (new Date).getTime();
+            Forms.push(form);
+            callback(form);
 
         };
 
-        FormService.findAllFormsForUser = function(userId, callback) {
+       FormService.findAllFormsForUser = function(userId, callback) {
 
+            var formIndex;
+            var userForms = [];
+            for(formIndex in Forms){
+                var form = Forms[formIndex];
+                if(userId === form.userId){
+                    userForms.push(form);
+                }
+            }
+            callback(userForms);
         };
 
         FormService.deleteFormById = function(formId, callback) {
 
+            var formIndex;
+            for(formIndex in Forms){
+                var form = Forms[formIndex];
+                if(formId === form._id){
+                    Forms.splice(formIndex, 1);
+                    callback(Forms);
+                }
+            }
         };
 
         FormService.updateFormById = function(formId, newForm, callback) {
 
+            var formIndex;
+            for(formIndex in Forms){
+                var form = Forms[formIndex];
+                if(formId === form._id){
+                    form = newForm;
+                    callback(form);
+                }
+            }
         };
 
 
--- public/assignment/views/forms/forms.controller.js
+++ public/assignment/views/forms/forms.controller.js
@@ -1,3 +1,25 @@
 /**
  * Created by Naveen on 2/25/2016.
  */
+(function() {
+    "use strict"
+    angular
+        .module("FormBuilderApp")
+        .controller("FormController", FormController);
+
+    function FormController($scope, $rootScope, $location, FormService) {
+
+        // Using the FormService, get the current array of forms for the currently
+        // logged in user and make them available for the view to render
+
+
+
+
+
+
+
+
+
+    }
+
+})();
